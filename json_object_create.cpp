b412d86ad0ea192e5464bf1ab98dfc01d93e6133
Sombaran Gupta <40040729+Sombaran@users.noreply.github.com>
2019-03-05 00:10:10 +0530

    Update json_object_create.cpp

--- json_object_create.cpp
+++ json_object_create.cpp
@@ -1,23 +1,26 @@
 /** Originally created for testing purpose. Made by Sombaran Gupta.
  * For any further query email me @ guptasombaran@yahoo.com
  * Created for Linux environment
+ * @Brief: JSON-C library used to convert json string into object
+ * prints value to each key
  * @return
  */
 
+ 
 #include <iostream>
-#include <stdio.h>
 #include <json-c/json.h>
 
 int main(void) {
-char *rec_msg = "{ \"fruit\": \"Apple\", \"size\": \"Large\", \"color\": \"Red\" }";
+const char *rec_msg = "{ \"fruit\": \"Apple\", \"size\": \"Large\", \"color\": \"Red\" }";
 json_object * obj = json_tokener_parse(rec_msg);
 std::cout << json_object_get_string(obj) << std::endl;
-json_object * name = json_object_object_get(obj,"bank");
-json_object * age = json_object_object_get(obj,"account_type");
-json_object * city = json_object_object_get(obj,"card_type");
+std::cout << json_object_get_string(json_object_object_get(obj,"fruit")) << std::endl;
+std::cout << json_object_get_string(json_object_object_get(obj,"size")) << std::endl;
+std::cout << json_object_get_string(json_object_object_get(obj,"color")) << std::endl;
 std::cout << json_object_get_string(obj) << std::endl;
 /* Free the JSON pointer */
 json_object_put(obj);
 return(0);
 }
+
 
